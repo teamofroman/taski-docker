da37c45c939787d94896b999b017d9ebaa9d586f
Роман Александров <rsateam@yandex.ru>
2024-10-26 18:15:21 +0000

    The secret variables are placed in the environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,14 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = (
-    'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
-)
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DJANGO_DEBUG', '1')))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
